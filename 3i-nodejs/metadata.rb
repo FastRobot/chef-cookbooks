7e977ab2fb874cb2a377d54fcec449cd98bb8691
lamont <lamont@fastrobot.com>
2020-03-05 09:27:25 -0800

    goodbye 3i-nodejs

--- 3i-nodejs/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             '3i-nodejs'
-maintainer       'Mike Lang'
-maintainer_email 'mike@thirdiron.com'
-license          'All rights reserved'
-description      'Installs/Configures 3i-nodejs'
-version          '0.2.0'
-
-depends 'nodejs', '~> 6.0.0'
