18e6b2606c135560395ba87156a1494fd84b98b0
lamont <lamont@fastrobot.com>
2020-03-05 17:18:56 -0800

    so long, herald_conf. your underscore made me sad but we will muddle through somehow

--- herald_conf/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'herald_conf'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures herald_conf'
-long_description 'Installs/Configures herald_conf'
-version          '0.1.0'
